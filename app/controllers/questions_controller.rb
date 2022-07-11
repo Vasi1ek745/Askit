ca4de75a5dac32083ce9dd9bfc19b4b6c7a5cac2
Vasi1ek745 <vasi1ek745@gmail.com>
2022-07-11 10:17:13 +0500

    do link show edit delete, in delete use form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show destroy edit update]
+
   def index
     @questions = Question.order(created_at: :desc)
   end
@@ -7,17 +9,47 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:success] = 'Question created!'
+      redirect_to questions_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @question.update(question_params)
+      flash[:success] = 'Question update!'
+      redirect_to questions_path(anchor: "question-#{@question.id}")
+    else
+      render :new
+    end
   end
 
   def destroy
+    if @question.delete
+      flash[:success] = 'Question delete!'
+      redirect_to questions_path
+    else
+      render :questions
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
